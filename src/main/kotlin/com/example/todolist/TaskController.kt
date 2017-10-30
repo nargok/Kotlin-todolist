6c4d42e66e8b97286294dc7533f4618a37ae925b
後閑諒一 <nargok@gmail.com>
2017-10-30 18:51:50 +0900

    update action

--- src/main/kotlin/com/example/todolist/TaskController.kt
+++ src/main/kotlin/com/example/todolist/TaskController.kt
@@ -1,5 +1,6 @@
 package com.example.todolist
 
+import javassist.NotFoundException
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PatchMapping
 
 @Controller
 @RequestMapping("tasks")
@@ -33,6 +36,29 @@ class TaskController(val taskRepository: TaskRepository) {
 
         val content = requireNotNull(form.content)
         taskRepository.create(content)
+        return "redirect:/tasks"
+    }
+
+    @GetMapping("{id}/edit")
+    fun edit(@PathVariable("id") id: Long,
+             form: TaskUpdateForm): String {
+        val task = taskRepository.findById(id) ?: throw NotFoundException()
+        form.content = task.content
+        form.done = task.done
+        return "tasks/edit"
+    }
+
+    @PatchMapping("{id}")
+    fun update(@PathVariable("id") id: Long,
+               @Validated form: TaskUpdateForm,
+               bindingResult: BindingResult): String {
+        if (bindingResult.hasErrors())
+            return "tasks/edit"
+
+        val task = taskRepository.findById(id) ?: throw NotFoundException()
+        val newTask = task.copy(content = requireNotNull(form.content),
+                done = form.done)
+        taskRepository.update(newTask)
         return "redirect:/tasks"
     }
 }
